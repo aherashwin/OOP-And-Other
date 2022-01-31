d292ccd27921eec6d0cc53b409a5b309cb59af15
Ashwini Aher <98300102+aherashwin@users.noreply.github.com>
2022-01-31 13:28:37 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,28 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	int[] x= new int[5];
+	//	System.out.println(x.getClass().getName());
+	//	int[][] x=new int[2][3];
+	//	System.out.println(x);
+	//	System.out.println(x[0]);
+	//    System.out.println(x[0][0]);
+	//	System.out.println(x[0][1]);
+	//	System.out.println(x[2][0]);
+	//	int[][]x= {{10,20,30,40},{50,60}};
+	//	for (int[] x2:x)
+	//	{
+	//		for (int x1: x2)
+	//			System.out.println(x1);
+	//	}
+	//	int[] a= new int[0];
+	//	System.out.println(a.length);
+		int[] x= new int[3];
+		System.out.println("x[0] is" + x[0]);
+		
+
+	}
+
+}
